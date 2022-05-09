e2e9634554c3413f1ded1489deda1e2860333b41
jjongyou <youjong12@naver.com>
2022-05-09 23:52:38 +0900

    add time axis

--- py_pid/pid_viewer.py
+++ py_pid/pid_viewer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import rclpy
+import time
 from rclpy.node import Node
 from ichthus_can_msgs.msg import Pid
 from std_msgs.msg import Float64
@@ -16,29 +17,30 @@ class Pid_graph(Node):
       Float64, "ref_vel", self.ref_callback, 10)
     self.whl_spd_subs = self.create_subscription(
       Float64MultiArray, "WHL_SPD11", self.whl_callback, 10)
+    self.start_time = time.time()
     self.whl_spd_axis = []
-    self.whl_index_axis = []
-    self.whl_index = 0
+    self.whl_time_axis = []
     self.ref_vel = 0
     self.ref_subs
     self.fig = plt.figure()
 
   def whl_callback(self, data): 
+    arrive_time = time.time()
+    time_index = arrive_time - self.start_time
     curr_spd = 0
     idx = 0
-    self.whl_index = self.whl_index + 1
     for idx in range (4):
       curr_spd = curr_spd + data.data[idx]
     curr_spd = curr_spd / 4
     self.whl_spd_axis.append(curr_spd)
-    self.whl_index_axis.append(self.whl_index)
+    self.whl_time_axis.append(time_index)
 
     #plt.yscale('linear')
     plt.xlabel("Time", fontsize=14)
     plt.ylabel("Wheel Velocity", fontsize=14)
     plt.axhline(self.ref_vel, color="red", linestyle="-", label="Ref")
     plt.legend()
-    plt.plot(self.whl_index_axis, self.whl_spd_axis, color="black")
+    plt.plot(self.whl_time_axis, self.whl_spd_axis, color="black")
     plt.draw()
     plt.pause(0.2)
     self.fig.clear()
